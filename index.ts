5fd22240633cb634f730c22f4131fe7b0f6f40c8
Glenn Ji <qiang.ji@yahoo.com.au>
2017-03-02 23:42:44 +1100

    clean up and add postage assessment calculator service

--- index.ts
+++ index.ts
@@ -1,37 +1,24 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpModule} from '@angular/http';
-import {SampleComponent} from './src/sample.component';
-import {SampleDirective} from './src/sample.directive';
-import {SamplePipe} from './src/sample.pipe';
-import {SampleService} from './src/sample.service';
 
-export * from './src/sample.component';
-export * from './src/sample.directive';
-export * from './src/sample.pipe';
-export * from './src/sample.service';
+import {PacService} from './src/postage-assessment-calculator.service';
+
+export * from './src/postage-assessment-calculator.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpModule
   ],
-  declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
-  ],
-  exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
-  ]
+  declarations: [],
+  exports: []
 })
-export class SampleModule {
+export class AustraliaPostAPIModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: AustraliaPostAPIModule,
+      providers: [PacService]
     };
   }
 }
---
+++ src/postage-assessment-calculator.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class PacService {
+
+  constructor() {}
+
+  test(): string {
+    return 'this is a test';
+  }
+}
